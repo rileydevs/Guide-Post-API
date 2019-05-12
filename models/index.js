f6812f271a75c5a414305b7ece7cd6c4a3fe47e4
unknown <you@example.com>
2019-05-11 23:08:24 -0400

    removed path

--- models/index.js
+++ models/index.js
@@ -1,10 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://rileydev:J24S17gr99!#@ds137687.mlab.com:37687/guide-post-api', 
+mongoose.connect('', 
     {useNewUrlParser: true});
 
 mongoose.Promise = Promise;
 
 module.exports.Post = require("./post");
-
-//mongodb://jriley:Express!1@ds221416.mlab.com:21416/express_api
