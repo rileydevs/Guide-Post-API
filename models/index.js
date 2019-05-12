97b798d69196550aacb706d587df779798d91d8d
JamesRileydev <37230562+JamesRileydev@users.noreply.github.com>
2019-05-11 23:11:33 -0400

    Delete index.js

--- models/index.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.set('debug', true);
-mongoose.connect('', 
-    {useNewUrlParser: true});
-
-mongoose.Promise = Promise;
-
-module.exports.Post = require("./post");
