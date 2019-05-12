35617e250e000c0ef228730ec2786521afa9dbb5
JamesRileydev <37230562+JamesRileydev@users.noreply.github.com>
2019-05-11 23:14:18 -0400

    Delete index.1.js

--- models/index.1.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.set('debug', true);
-mongoose.connect('mongodb://rileydev:J24S17gr99!#@ds137687.mlab.com:37687/guide-post-api', 
-    {useNewUrlParser: true});
-
-mongoose.Promise = Promise;
-
-module.exports.Post = require("./post");
